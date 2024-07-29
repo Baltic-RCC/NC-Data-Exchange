61de64d2fa6d0e66858245ee803d35737ded8057
martynas.karobcikas <martynas.karobcikas@baltic-rcc.eu>
2024-07-29 15:37:58 +0300

    aligned pypirc file

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 python setup.py sdist bdist_wheel
-python -m twine upload --repository gitlab --skip-existing dist/* --verbose --cert C:\\Install\\caroot.crt
+python -m twine upload --repository gitlab-nc-data-exchange --skip-existing dist/* --verbose --cert C:\\Install\\caroot.crt
 exec bash
