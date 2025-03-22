0e16c9701fd44db6572788dcd2ac9713c1fa4e15
martynas.karobcikas <martynas.karobcikas@baltic-rcc.eu>
2025-03-22 21:58:58 +0200

    feature: rdfxml to json converted

--- nc_data_exchange/rdfxml_to_jsonld.py
+++ nc_data_exchange/rdfxml_to_jsonld.py
@@ -1,8 +1,9 @@
 import rdflib
 import json
+import xmltodict
 
 
-def rdfxml_to_json(rdfxml_path, json_path):
+def rdfxml_to_jsonld(rdfxml_path, json_path):
 
     g = rdflib.Graph()
     g.parse(rdfxml_path, format='xml')
@@ -19,6 +20,20 @@ def rdfxml_to_json(rdfxml_path, json_path):
         json.dump(json_dict, f, indent=4)
 
 
+def rdfxml_to_json(rdfxml_path, json_path):
+
+    with open(rdfxml_path, 'r') as f:
+        xml_content = f.read()
+
+    json_dict = xmltodict.parse(xml_content)
+
+    with open(json_path, 'w') as f:
+        json.dump(json_dict, f, indent=4)
+
+
 if __name__ == '__main__':
     # Test
-    pass
+    CONTINGENCY_FILE_PATH = r"C:\Users\martynas.karobcikas\Documents\python_projects\nc-data-exchange\tests\test-data\TC1_contingencies.xml"
+    OUTPUT_PATH = r"C:\Users\martynas.karobcikas\Documents\python_projects\nc-data-exchange\tests\test-data\TC1_contingencies.json"
+    rdfxml_to_jsonld(rdfxml_path=CONTINGENCY_FILE_PATH, json_path=OUTPUT_PATH)
+    rdfxml_to_json(rdfxml_path=CONTINGENCY_FILE_PATH, json_path=OUTPUT_PATH)
