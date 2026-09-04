3651e82cd4b6bb633f293999d42a1e11bcc7ef5e
lukas.navickas <lukas.navickas@baltic-rcc.eu>
2026-09-04 11:18:37 +0300

    feat: added base class for RemedialActionScheduleResponse, aligned enums for RA status/rejection reason according to NC specification

--- nc_data_exchange/profiles/Enumerations.py
+++ nc_data_exchange/profiles/Enumerations.py
@@ -42,6 +42,27 @@ class RemedialActionScheduleStatusKind(Enum):
         return f"https://cim4.eu/ns/nc#RemedialActionScheduleStatusKind.{self.value}"
 
 
+class RemedialActionScheduleResponseKind(Enum):
+    accepted = "accepted"
+    refused = "refused"
+    timeout = "timeout"
+    waiting = "waiting"
+
+    def __str__(self):
+        return f"https://cim4.eu/ns/nc#RemedialActionScheduleResponseKind.{self.value}"
+
+
+class RejectionReasonKind(Enum):
+    madeAlternative = "madeAlternative"
+    noTimeToImplement = "noTimeToImplement"
+    notAvailable = "notAvailable"
+    securityLimit = "securityLimit"
+    other = "other"
+
+    def __str__(self):
+        return f"https://cim4.eu/ns/nc#RejectionReasonKind.{self.value}"
+
+
 class ValueOffsetKind(Enum):
     absolute = "absolute"
     incremental = "incremental"
